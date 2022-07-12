eb87e6fc72bd30955379aaf1ceb51a7ed3f57937
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-12 18:53:21 +0530

    Time: 2 ms (66.61%), Space: 5.8 MB (95.63%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int n) {
+        long long int ans = 0;
+        while(n!=0)
+        {
+            int digit = n% 10;
+            if ( (ans > INT_MAX/10) || (ans < INT_MIN/10) )
+                return 0;
+            ans = (ans*10)+digit;
+            n = n/10;
+        }
+        
+            return ans;
+        
+    }
+};
