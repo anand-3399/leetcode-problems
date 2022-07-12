928476048ea30ef1c9bc1157502144a4fe1c5182
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-12 21:57:34 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.52%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,7 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         
         if(n==0) return false;
-        while(n%2==0) n/=2;
+        while(n%2==0) 
+            n/=2;
         return n==1;
     }
 };
