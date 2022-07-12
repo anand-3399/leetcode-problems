afa8dc9235547c498ca72ab396288712bcddeb23
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-12 21:55:51 +0530

    Time: 5 ms (23.93%), Space: 5.9 MB (70.06%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -5,8 +5,7 @@ public:
         int ans = 1;
         for(int i=0; i<=30; i++){
             if(ans == n) return true;
-            if(ans < INT_MAX/2)
-            ans = ans * 2;
+            if(ans < INT_MAX/2) ans = ans * 2;
         }
 
         return false;
