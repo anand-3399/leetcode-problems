210c02030d4c75ea2c8ad36af90bfd0602b94f10
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-14 14:22:12 +0530

    Time: 4 ms (45.65%), Space: 8.7 MB (91.89%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -5,8 +5,7 @@ public:
         int k=0;
         for(int i=0; i<nums.size(); i++)
         {
-            if(nums[i] == val)
-                continue;
+            if(nums[i] == val) continue;
             else{
                 nums[k] = nums[i];
                 k += 1;
