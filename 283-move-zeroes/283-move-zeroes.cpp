1cb7977fb9e1b2c8c53462330e065a2b1f4285cb
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-14 22:35:48 +0530

    Time: 26 ms (85.08%), Space: 19.1 MB (83.28%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -4,18 +4,16 @@ public:
         // To point to the zero 
         int zeroindex = 0;
         
-        for(int i=0 ; i< nums.size() ; i++)
+        for(int i=0 ; i< nums.size() ; i+=1)
         {
             // if number is not zero then swap it with index having number zero and increment zero index 
             if(nums[i] != 0)
             {
                 swap(nums[i] , nums[zeroindex]);
-                zeroindex++;
+                zeroindex+=1;
             }
         }
         
-        // at end all zeros will be swapped to end
-        // try to run 2 test cases for better understanding
         
     }
 };
