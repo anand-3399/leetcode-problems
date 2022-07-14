6a726d07c5c23062df76453f3645fb811305bb0a
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-14 22:08:02 +0530

    Time: 48 ms (10.18%), Space: 18 MB (63.93%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -4,10 +4,9 @@ public:
         
         sort(nums.begin(),nums.end());
         for(int i = 0;i < nums.size();i++)
-        {
-            cout<<nums[i]<<" ";
-            if(nums[i] != i) return i;
-        }
+            if(nums[i] != i) 
+                return i;
+        
         
         return nums.size();
     }
