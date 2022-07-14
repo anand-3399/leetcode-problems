8c35c0152e818a207e2fafa079a41e9366dfbbe4
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-14 14:19:03 +0530

    Time: 9 ms (7.96%), Space: 8.8 MB (35.10%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,20 +1,13 @@
 #include <bits/stdc++.h>
 class Solution {
 public:
-    int removeElement(vector<int>& arr, int val) {
-        int size = 0;
+    int removeElement(vector<int>& nums, int val) {
         int j = 0;
-        for(int i=0; i<arr.size(); i++)
-        {
-            // cout<<arr[i]<<" ";
-            if(arr[i] != val)
-            {
-                cout<<arr[i]<<" " <<i <<endl;
-                arr[j] = arr[i];
-                size+=1;
-                j+=1;
-            }
+        for (int i = 0, l = nums.size(); i < l; i++) {
+            if (nums[i] != val)
+                nums[j++] = nums[i];
         }
-        return size;
+        
+        return j;
     }
 };
