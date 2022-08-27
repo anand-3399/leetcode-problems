5cc19dca3dbdae29fd5e79a6ea76202d7439de67
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-08-27 21:51:47 +0530

    Time: 52 ms (21.45%), Space: 19.4 MB (13.44%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // To point to the zero 
-        int zeroindex = 0;
-        
-        for(int i=0 ; i< nums.size() ; i+=1)
-        {
-            // if number is not zero then swap it with index having number zero and increment zero index 
-            if(nums[i] != 0)
-            {
-                swap(nums[i] , nums[zeroindex]);
-                zeroindex+=1;
+        int c = 0;
+        for (int i=0; i<nums.size(); i++)
+            if(nums[i] != 0){
+                swap(nums[i],nums[c]);
+                c++;
             }
-        }
-        
-        
     }
 };
