83268dda7e7d314a171e06642b18964d5d53dc6b
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-08-27 21:54:03 +0530

    Time: 36 ms (57.35%), Space: 19.1 MB (98.10%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -4,13 +4,12 @@ public:
         int zeroindex = 0;
         
         for(int i=0 ; i< nums.size() ; i+=1)
-        {
             if(nums[i] != 0)
             {
                 swap(nums[i] , nums[zeroindex]);
                 zeroindex+=1;
             }
-        }
+        
         
         
     }
