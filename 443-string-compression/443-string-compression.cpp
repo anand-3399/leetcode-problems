c6563193440739300cb25e2c1e339e44e035696c
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-09-01 15:31:11 +0530

    Time: 12 ms (23.31%), Space: 9 MB (67.76%) - LeetHub

--- 443-string-compression/443-string-compression.cpp
+++ 443-string-compression/443-string-compression.cpp
@@ -5,9 +5,8 @@ public:
         int n = chars.size();
         while(i<n){
             int j = i+1;
-            while(j<n && chars[i] == chars[j]){
-                j+=1;
-            }
+            while(j<n && chars[i] == chars[j]) j+=1;
+            
             
             // ya toh vector pura traverse krr diyaa
             // ya fir new/Different character encounter kia hai
@@ -17,9 +16,8 @@ public:
             if(count > 1){
                 // converting counting into single digit and saving in answer
                 string cnt = to_string(count);
-                for(char ch: cnt){
-                    chars[ansIndex++] = ch;
-                }
+                for(char ch: cnt) chars[ansIndex++] = ch;
+                
             }
             i = j;
         }
