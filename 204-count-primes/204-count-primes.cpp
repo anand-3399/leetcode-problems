b6c57f2e6de09b10d4cb97bddae205ac1f0149b9
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-09-02 21:16:30 +0530

    Time: 409 ms (29.51%), Space: 10.2 MB (70.29%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        
         int cnt = 0;
+        
         vector<bool> prime(n+1, true);
         
         prime[0] = prime[1] = false;
@@ -9,10 +11,13 @@ public:
         for(int i=2; i<n; i++){
             
             if(prime[i]){
+                
                 cnt++;
             
                 for(int j=2*i; j<n; j = j+i){
+                    
                     prime[j] = 0;
+                    
                 }
             }
             
