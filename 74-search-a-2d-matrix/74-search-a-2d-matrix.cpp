c50e5f0ef5127355c1c6236ff426480bfb98ffa3
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-09-02 19:05:55 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (51.39%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -7,8 +7,6 @@ public:
         int start = 0;
         int end = row*col - 1;
         
-        
-        
         while(start<=end){
             int mid = start + (end - start)/2;
             int element = matrix[mid/col][mid%col];
