e9bea608d374b580da384c92790d414ccc31baac
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-09-02 16:43:32 +0530

    Time: 4 ms (41.74%), Space: 7.1 MB (33.49%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -5,15 +5,15 @@ public:
         int n = matrix.size();
         int m = n;
         int ans[n][n];
-        for (int row = 0; row < n; row++)
-        {
-            for (int col = 0; col < m; col++)
-            {
+        for (int row = 0; row < n; row+=1)
+        
+            for (int col = 0; col < m; col+=1)
+            
                 ans[col][m - row - 1] = matrix[row][col];
-            }
-        }
-        for(int i=0; i<n;i++)
-            for(int j=0;j<m;j++)
+            
+        
+        for(int i=0; i<n;i+=1)
+            for(int j=0;j<m;j+=1)
                 matrix[i][j] = ans[i][j];
         
     }
