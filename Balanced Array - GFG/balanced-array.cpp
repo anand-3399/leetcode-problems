b08bf33be6aac8e0a8941a1953bcb058c756ae5d
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-16 23:33:55 +0530

    Added solution - LeetHub

---
+++ Balanced Array - GFG/balanced-array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int minValueToBalance(int a[], int n)
+    {
+       //code here.
+       int mid=n/2;
+       int sum1=0, sum2=0;
+       for(int i=0; i<mid; i++)
+       {
+           sum1+=a[i];
+           sum2+=a[mid+i];
+       }
+       
+       return sum1>sum2?(sum1-sum2):(sum2-sum1);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;	
+		cout<<ob.minValueToBalance(a,n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
