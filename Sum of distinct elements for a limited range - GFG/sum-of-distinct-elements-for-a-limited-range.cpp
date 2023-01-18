9c65a2bbda9e49b0c26093fb5c6f8916e1db8139
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-19 00:15:20 +0530

    Added solution - LeetHub

---
+++ Sum of distinct elements for a limited range - GFG/sum-of-distinct-elements-for-a-limited-range.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	int sumOfDistinct(int arr[], int n) 
+	{ 
+	    // Your code goes here
+	    set<int> set(arr, arr+n);
+	    int sum=0;
+	    for(auto &i:set)
+	        sum+=i;
+	    return sum;
+	} 
+		 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+	    cin >> n;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    
+
+       
+
+        Solution ob;
+        cout << ob.sumOfDistinct(a,n);
+	   
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
