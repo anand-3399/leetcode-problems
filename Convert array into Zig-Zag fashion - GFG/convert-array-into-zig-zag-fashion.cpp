eebe0d0992e0e67fce4fe599c8598e8127e08eb6
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-18 23:17:16 +0530

    Added solution - LeetHub

---
+++ Convert array into Zig-Zag fashion - GFG/convert-array-into-zig-zag-fashion.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Program for zig-zag conversion of array
+    void zigZag(int arr[], int n) {
+        // code here
+        // int max=*max_element(arr, arr+n);
+        // int min=*min_element(arr, arr+n);
+        for(int i=0; i<n; i++)
+        {
+            if(i%2==0)
+                arr[i]=0;
+            else
+                arr[i]=1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+bool isZigzag(int arr[], int n){
+    int f=1;
+    
+    for(int i=1; i<n; i++){
+        if(f){
+            if(arr[i-1]>arr[i]) return 0;
+        }
+        else{
+            if(arr[i-1]<arr[i]) return 0;
+        }
+        f=f^1;
+    }
+    
+    return 1;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.zigZag(arr, n);
+        bool check=1;
+        check=isZigzag(arr,n);
+        
+        if(check) cout<<"1\n";
+        else cout<<"0\n";
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
