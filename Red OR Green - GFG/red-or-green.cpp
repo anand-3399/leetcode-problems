0b196fb3d2a6dea123d8bcc510145cf683fc3ba6
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-20 21:30:09 +0530

    Added solution - LeetHub

---
+++ Red OR Green - GFG/red-or-green.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+       int RedOrGreen(int n,string s) {
+           //code here
+        int c1 = count(s.begin(), s.end(), 'R');
+        int c2 = count(s.begin(), s.end(), 'G');
+        
+        return min(c1,c2);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.RedOrGreen(N,S) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
