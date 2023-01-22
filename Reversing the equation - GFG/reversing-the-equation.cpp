160de0e29410a2cfae127ec7cc309a16267e807a
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-23 00:04:25 +0530

    Added solution - LeetHub

--- Reversing the equation - GFG/reversing-the-equation.cpp
+++ Reversing the equation - GFG/reversing-the-equation.cpp
@@ -10,16 +10,19 @@ class Solution
     string reverseEqn (string s)
     {
         // code here
-        s.push_back('#');
+        s.push_back('#');   // as number is taken from this SYMBOL only
         vector<string> v;
-        int begin = 0;
+        int begin = 0;      // this is the number/character beginning index
         for(int i=0; i<s.size(); i++)
         {
             if(s[i] < '0')
             {
+                // First we push the number or character present
                 v.push_back(s.substr(begin, i - begin));
                 begin = i+1;
                 
+                // Now we push the symbol. As we cannot directly 
+                // push char to string vector, we convert char to string
                 string temp = "";
                 temp += s[i];
                 v.push_back(temp);
@@ -27,6 +30,7 @@ class Solution
         }
         
         string ans="";
+        // the last string contains # as an extra symbol which we have to ignore it.
         for(int i=v.size()-2; i>=0; i--)
             ans += v[i];
             
