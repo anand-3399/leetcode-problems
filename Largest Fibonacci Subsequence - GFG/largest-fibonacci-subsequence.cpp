b05d8f534814dc023e6525981703fb99b61657d3
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-24 21:10:23 +0530

    Added solution - LeetHub

--- Largest Fibonacci Subsequence - GFG/largest-fibonacci-subsequence.cpp
+++ Largest Fibonacci Subsequence - GFG/largest-fibonacci-subsequence.cpp
@@ -13,11 +13,10 @@ class Solution{
         int i;
         f[0] = 0;
         f[1] = 1;
-     
+        
         for(i = 2; i <= n; i++)
-        {
            f[i] = f[i - 1] + f[i - 2];
-        }
+        
         return f;
     }
     
