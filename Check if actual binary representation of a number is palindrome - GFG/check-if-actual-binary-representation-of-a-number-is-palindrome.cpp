c4563d4c4d73d606e6254678ae51c6795c7d034e
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-26 22:57:35 +0530

    Added solution - LeetHub

---
+++ Check if actual binary representation of a number is palindrome - GFG/check-if-actual-binary-representation-of-a-number-is-palindrome.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int binaryPalin (long long int n)
+	{
+	    // Your Code Here
+	    string s = bitset<128>(n).to_string().substr(128 - log2(n));
+	    string temp=s;
+	    reverse(temp.begin(), temp.end());
+	    return (temp == s)?1:0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.binaryPalin (n) << endl;
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
